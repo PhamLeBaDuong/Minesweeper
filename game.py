ed98e1fb336d5f41f3615611aab8c648db9bade5
PhamLeBaDuong <108616528+PhamLeBaDuong@users.noreply.github.com>
2023-12-24 22:49:07 +0000

    Fixed printing errors and added input checking

--- game.py
+++ game.py
@@ -1,9 +1,16 @@
 import random
 
-def print_board(board):
-    for row in board:
-        print(" ".join(row))
+def print_board(board, revealed):
+    for i in range(len(board)):
+        row_str = ""
+        for j in range(len(board[0])):
+            if revealed[i][j]:
+                row_str += str(board[i][j]) + " "
+            else:
+                row_str += "  "
+        print(row_str.strip())
     print()
+
 
 def initialize_board(rows, cols, mines):
     board = [[' ' for _ in range(cols)] for _ in range(rows)]
@@ -41,17 +48,23 @@ def minesweeper(rows, cols, mines):
     revealed = [[False for _ in range(cols)] for _ in range(rows)]
 
     while True:
-        print_board(revealed)
+        print_board(board, revealed)
 
-        row = int(input("Enter row: "))
-        col = int(input("Enter column: "))
+        row = input("Enter row: ")
+        while len(row)==0:
+            row = input("Enter the valid row number: ")
+        row = int(row)
+        col = input("Enter column: ")
+        while len(col)==0:
+            col = input("Enter the valid column number: ")
+        col = int(col)
 
         if not (0 <= row < rows and 0 <= col < cols):
             print("Invalid input. Please enter valid row and column.")
             continue
 
         if board[row][col] == '*':
-            print_board(board)
+            print_board(board, revealed)
             print("Game Over! You hit a mine.")
             break
         else:
@@ -60,13 +73,22 @@ def minesweeper(rows, cols, mines):
             reveal(board, revealed, row, col)
 
             if all(all(revealed[i][j] or board[i][j] == ' ' for j in range(cols)) for i in range(rows)):
-                print_board(board)
+                print_board(board, revealed)
                 print("Congratulations! You win!")
                 break
 
 if __name__ == "__main__":
-    rows = int(input("Enter number of rows: "))
-    cols = int(input("Enter number of columns: "))
-    mines = int(input("Enter number of mines: "))
+    rows = input("Enter number of rows: ")
+    while len(rows)==0:
+        rows = input("Enter the valid number of rows: ")
+    rows = int(rows)
+    cols = input("Enter number of columns: ")
+    while len(cols)==0:
+        cols = input("Enter the valid number of columns: ")
+    cols = int(cols)
+    mines = input("Enter number of mines: ")
+    while len(mines)==0:
+        mines = input("Enter the valid number of mines: ")
+    mines = int(mines)
 
     minesweeper(rows, cols, mines)
